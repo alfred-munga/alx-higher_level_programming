8e57434fbd87e1f31b466bf05863e015e38446f4
alfred-munga <alfred_munga@yahoo.com>
2024-03-19 17:00:06 +0300

    Refactor print statements in 1-last_digit.py to use format() method

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
 if last_digit > 5:
-    print(f"Last digit of {last_digit} is {last_digit} and is greater than 5")
+    print("Last digit of {digit:d} is {digit:d} and is greater than 5".format(digit=last_digit))
 elif last_digit == 0:
-    print(f"Last digit of {last_digit} is {last_digit} and is 0")
+    print("Last digit of {digit:d} is {digit:d} and is 0".format(digit=last_digit))
 else:
-    print(f"Last digit of {last_digit} is {last_digit} and is less than 6 and not 0")
+    print("Last digit of {digit:d} is {digit:d} and is less than 6 and not 0".format(digit=last_digit))
